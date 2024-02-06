bf77884f2fe4ff471945fe0310079e26b1d36db8
culplate <culplate@gmail.com>
2024-02-06 13:12:37 +0100

    components added + markup for searchBar

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const API_KEY = "gOTmj8JmQWGgiHpU4pzX2JYCN-uvE_TdR5QD1-gJgZM";
   const [count, setCount] = useState(0);
 
   return <></>;
---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+import css from "./SearchBar.module.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+export const SearchBar = () => {
+  return (
+    <header className={css.head}>
+      <Formik>
+        <Form className={css.searchForm}>
+          <Field
+            className={css.searchInput}
+            type="text"
+            name="search"
+            autoComplte="off"
+          />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+    </header>
+  );
+};
