d36f9febe0e4a28a33bfc1a0427b28a32409353e
culplate <culplate@gmail.com>
2024-02-11 21:37:16 +0100

    added lazyload

--- src/Components/ImageCard/ImageCard.jsx
+++ src/Components/ImageCard/ImageCard.jsx
@@ -1,9 +1,10 @@
 import css from "./ImageCard.module.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const ImageCard = ({ imgData }) => {
   return (
     <div className={css.wrapper}>
-      <img
+      <LazyLoadImage
         className={css.galleryImg}
         alt={imgData.alt_description}
         src={imgData.urls.small}
